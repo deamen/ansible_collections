61d30236abdc6a0fb4c79dc1c99963d34f43de7f
Song Tang <stang@mmz.au>
2024-12-18 18:30:46 +1100

    gh-9 Add working code

---
+++ collections/ansible_collections/deamen/certificate/plugins/action/deploy_private_ca.py
@@ -0,0 +1,48 @@
+from ansible.plugins.action import ActionBase
+from .deploy_certificate import ActionModule as CopyAction
+
+class ActionModule(ActionBase):
+    """
+    Action plugin for deploy_private_ca that reuses deploy_certificate plugin.
+    """
+
+    def run(self, tmp=None, task_vars=None):
+        # Extract parameters passed to the task
+        private_ca = self._task.args.get("private_ca")
+        filename = self._task.args.get("filename", "custom-ca.crt")
+
+        self._display.v(f"filename: {filename}")
+        if not private_ca:
+            return {"failed": True, "msg": "'private_ca' parameter is required."}
+
+        # Define parameters for deploy_certificate plugin
+        ca_trust_dir = "/etc/pki/ca-trust/source/anchors/"
+        deploy_certificate_args = {
+            "name": filename,
+            "cert_content": private_ca,
+            "cert_dir": ca_trust_dir,
+            "cert_owner": "root",
+            "cert_group": "root",
+            "cert_mode": "0644",
+            "is_ca": True
+        }
+
+        self._task.args.update(deploy_certificate_args)
+        # Invoke the deploy_certificate action plugin
+        deploy_certificate_action = CopyAction(
+            self._task,
+            self._connection,
+            self._play_context,
+            self._loader,
+            self._templar,
+            self._shared_loader_obj,
+        )
+
+        result = deploy_certificate_action.run(tmp=tmp, task_vars={**task_vars, **deploy_certificate_args})
+
+        module_args={}
+        # Call the module to run the update-ca-trust command
+        module_result = self._execute_module(module_name=self._task.action, module_args=module_args, task_vars=task_vars)
+        result.update(module_result)
+
+        return result
--- collections/ansible_collections/deamen/certificate/plugins/modules/deploy_private_ca.py
+++ collections/ansible_collections/deamen/certificate/plugins/modules/deploy_private_ca.py
@@ -3,6 +3,8 @@
 # Copyright: (c) 2024 Song Tang github.com/deamen
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from ansible.module_utils.basic import AnsibleModule
+
 DOCUMENTATION = """
 ---
 module: deploy_private_ca
@@ -10,34 +12,20 @@ short_description: Deploy a private CA certificate to a Linux server
 description:
   - Writes the content of a private CA certificate to the system's trust store directory.
   - Updates the system trust store using the `update-ca-trust` command.
-  - "Ensures the task runs only on supported Linux distributions: AlmaLinux, CentOS, RedHat, Rocky, or Fedora."
-version_added: "1.0.0"
 options:
   private_ca:
-    description:
-      - The content of the private CA certificate in PEM format.
+    description: The content of the private CA certificate in PEM format.
     required: true
     type: str
   filename:
-    description:
-      - The name of the file to save the private CA certificate as.
-      - Defaults to 'custom-ca.crt'.
-    required: false
+    description: The filename for the CA certificate.
+    default: custom-ca.crt
     type: str
-    default: 'custom-ca.crt'
-author:
-  - Your Name (@your_github_handle)
+author: Song Tang (@deamen)
 """
 
 EXAMPLES = """
-- name: Deploy a private CA certificate with default filename
-  deploy_private_ca:
-    private_ca: |
-      -----BEGIN CERTIFICATE-----
-      MIIDXTCCAkWgAwIBAgIJAL8AO9lD...
-      -----END CERTIFICATE-----
-
-- name: Deploy a private CA certificate with a custom filename
+- name: Deploy a private CA certificate
   deploy_private_ca:
     private_ca: |
       -----BEGIN CERTIFICATE-----
@@ -46,62 +34,28 @@ EXAMPLES = """
     filename: my-custom-ca.crt
 """
 
-from ansible.module_utils.basic import AnsibleModule
-import os
-import platform
-
-
 def main():
-    module_args = {
-        "private_ca": {"type": "str", "required": True},
-        "filename": {"type": "str", "default": "custom-ca.crt"},
-    }
+    # Define module arguments
+    module_args = {}
 
     # Initialize the Ansible module
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
-    private_ca = module.params["private_ca"]
-    filename = module.params["filename"]
-    ca_trust_path = "/etc/pki/ca-trust/source/anchors/"
+    # Command to update the system trust store
     update_command = "update-ca-trust"
-    dest_file = os.path.join(ca_trust_path, filename)
 
-    # Check if the OS is supported
-    supported_distros = ["AlmaLinux", "CentOS", "RedHat", "Rocky", "Fedora"]
-    distro = platform.system_alias(*platform.uname()[:3]).split()[
-        0
-    ]  # A more robust approach to get the distribution name
+    # Run the command and capture results
+    rc, out, err = module.run_command(update_command)
 
-    if distro not in supported_distros:
-        module.fail_json(
-            msg=f"Unsupported distribution: {distro}. This module supports: {', '.join(supported_distros)}."
-        )
+    # Handle errors if the command fails
+    if rc != 0:
+        module.fail_json(msg=f"Failed to run '{update_command}'. Error: {err}")
 
-    # Check if the CA trust directory exists
-    if not os.path.isdir(ca_trust_path):
-        module.fail_json(
-            msg=f"CA trust directory '{ca_trust_path}' does not exist. Please ensure the directory is present."
-        )
-
-    try:
-        # Write the certificate content to the destination file
-        with open(dest_file, "w") as f:
-            f.write(private_ca)
-
-        # Run update-ca-trust
-        rc, out, err = module.run_command(update_command)
-        if rc != 0:
-            module.fail_json(msg=f"Failed to run '{update_command}'. Error: {err}")
-
-        # Return success
-        module.exit_json(
-            changed=True,
-            msg=f"Private CA deployed successfully to {dest_file} and trust store updated.",
-        )
-
-    except Exception as e:
-        module.fail_json(msg=str(e))
-
+    # Exit successfully if the command runs without issues
+    module.exit_json(
+        changed=True,
+        msg="Private CA deployed successfully and trust store updated.",
+    )
 
 if __name__ == "__main__":
     main()
