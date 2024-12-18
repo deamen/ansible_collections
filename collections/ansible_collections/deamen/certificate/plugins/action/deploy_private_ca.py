86c972e242e1a4d46753e356ff4b14d9e8c09154
Song Tang <stang@mmz.au>
2024-12-18 18:35:01 +1100

    GH-9 Improve readability

--- collections/ansible_collections/deamen/certificate/plugins/action/deploy_private_ca.py
+++ collections/ansible_collections/deamen/certificate/plugins/action/deploy_private_ca.py
@@ -1,9 +1,10 @@
 from ansible.plugins.action import ActionBase
-from .deploy_certificate import ActionModule as CopyAction
+from .deploy_certificate import ActionModule as DeployCertificateAction
+
 
 class ActionModule(ActionBase):
     """
-    Action plugin for deploy_private_ca that reuses deploy_certificate plugin.
+    Action plugin for deploy_private_ca that reuses the deploy_certificate plugin.
     """
 
     def run(self, tmp=None, task_vars=None):
@@ -11,11 +12,14 @@ class ActionModule(ActionBase):
         private_ca = self._task.args.get("private_ca")
         filename = self._task.args.get("filename", "custom-ca.crt")
 
-        self._display.v(f"filename: {filename}")
+        # Debug output for filename
+        self._display.v(f"Filename: {filename}")
+
+        # Validate required parameters
         if not private_ca:
             return {"failed": True, "msg": "'private_ca' parameter is required."}
 
-        # Define parameters for deploy_certificate plugin
+        # Define parameters for the deploy_certificate plugin
         ca_trust_dir = "/etc/pki/ca-trust/source/anchors/"
         deploy_certificate_args = {
             "name": filename,
@@ -24,12 +28,14 @@ class ActionModule(ActionBase):
             "cert_owner": "root",
             "cert_group": "root",
             "cert_mode": "0644",
-            "is_ca": True
+            "is_ca": True,
         }
 
+        # Update task arguments with deploy_certificate parameters
         self._task.args.update(deploy_certificate_args)
+
         # Invoke the deploy_certificate action plugin
-        deploy_certificate_action = CopyAction(
+        deploy_certificate_action = DeployCertificateAction(
             self._task,
             self._connection,
             self._play_context,
@@ -37,12 +43,18 @@ class ActionModule(ActionBase):
             self._templar,
             self._shared_loader_obj,
         )
+        result = deploy_certificate_action.run(
+            tmp=tmp, task_vars={**task_vars, **deploy_certificate_args}
+        )
 
-        result = deploy_certificate_action.run(tmp=tmp, task_vars={**task_vars, **deploy_certificate_args})
+        # Execute the update-ca-trust command using a module
+        module_result = self._execute_module(
+            module_name=self._task.action,
+            module_args={},
+            task_vars=task_vars,
+        )
 
-        module_args={}
-        # Call the module to run the update-ca-trust command
-        module_result = self._execute_module(module_name=self._task.action, module_args=module_args, task_vars=task_vars)
+        # Merge the results from deploy_certificate and update-ca-trust
         result.update(module_result)
 
         return result
